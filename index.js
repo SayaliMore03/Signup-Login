d7860a3a067e744e7dd85127bb0761b6ffb00f04
Sayali More <163543210+SayaliMore03@users.noreply.github.com>
2025-11-14 13:46:22 +0530

    Added login function

--- index.js
+++ index.js
@@ -13,3 +13,25 @@ function signup(userName) {
 // Test
 console.log(signup("rohan")); // User Already Registered
 console.log(signup("aditi")); // Signup Successful
+
+
+// LOGIN FUNCTION
+function login(userName, password) {
+    let users = ["sayali", "rohan", "priya"]; // existing users
+    let correctPassword = "Emp@123";
+
+    if (!users.includes(userName)) {
+        return "User Not Found, Please Signup";
+    } 
+    else if (password !== correctPassword) {
+        return "Wrong Password....";
+    } 
+    else {
+        return "Login Successful...";
+    }
+}
+
+// Test
+console.log(login("sayali", "Emp@123")); // Login Successful
+console.log(login("sayali", "abc"));     // Wrong Password
+console.log(login("aditi", "Emp@123"));  // User Not Found
